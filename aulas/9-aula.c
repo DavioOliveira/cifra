787cf646b3b1aef73a797f32530827c596d7f30d
davi oliveira <franciscodavi@alu.ufc.br>
2021-11-08 18:03:32 -0300

    inicio01

---
+++ aulas/9-aula.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    char nome[20] = "russas";
+    printf("nome = %s\n",nome);
+
+    return 0;
+}
---
+++ aulas/aula-10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#define  N 5
+int main(){
+    int numb[N];
+    printf("NUMEROS = ");
+    for (int i = 0; i < N; i++){
+        printf("%d, ",numb[i]);
+    }
+    printf("\n");
+}
